cb4fa8b0b4b943e900e13f6d3a089ccd905acf1a
Павел Седой <pavliksedoy@gmail.com>
2019-04-25 17:39:24 +0300

    Home Slider in work

--- wp-content/themes/twentynineteen/js/sliders.js
+++ wp-content/themes/twentynineteen/js/sliders.js
@@ -13,3 +13,29 @@ $(".contacts__slider").hover(function(){
 }, function(){
     mySwiper.autoplay.start();
 });
+
+//  Home Page Header Slider
+var mySwiper = new Swiper ('.home-slider__slider', {
+    direction: 'horizontal',
+    loop: true,
+    slidesPerView: 1,
+    // autoplay: {
+    //     delay: 5000,
+    // },
+
+    // Navigation arrows
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+    effect: "slide",
+    speed: 800,
+    pagination: {
+        el: '.header-slider-pagination',
+        type: 'bullets',
+        formatFractionCurrent: function(number) { return number},
+        renderBullet: function (index, className) {
+            return '<span class="' + className + '">' + (index + 1) + '</span>';
+        }
+    }
+});
--- wp-content/themes/twentynineteen/page-home.php
+++ wp-content/themes/twentynineteen/page-home.php
@@ -9,7 +9,44 @@
 get_header();
 ?>
 
-    AVTO
+<div class="home-slider">
+    <div class="home-slider__slider">
+        <div class="swiper-wrapper">
+            <div class="swiper-slide home-slider__slide">
+                <div class="home-slider__slide-bg">
+                    <img src="<?= get_stylesheet_directory_uri() ?>/images/header.jpg" class="home-slider__slide-bg_img">
+                </div>
+                <div class="home-slider__content_wr">
+                    <div class="home-slider__content container">
+                        <div class="home-slider__title">
+                            <h1>Пригон авто из США под ключ</h1>
+                        </div>
+                        <div class="home-slider__btn_wr">
+                            <a href="/index.php/avto/" class="home-slider__btn">Смотреть Все авто</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="swiper-slide home-slider__slide">
+                <div class="home-slider__slide-bg">
+                    <img src="<?= get_stylesheet_directory_uri() ?>/images/header2.jpg" class="home-slider__slide-bg_img">
+                </div>
+            </div>
+            <div class="swiper-slide home-slider__slide">
+                <div class="home-slider__slide-bg">
+                    <img src="<?= get_stylesheet_directory_uri() ?>/images/header3.jpg" class="home-slider__slide-bg_img">
+                </div>
+            </div>
+        </div>
+
+        <div class="home-slider__nav">
+            <div class="swiper-button-prev"></div>
+            <div class="swiper-button-next"></div>
+        </div>
+
+        <div class="header-slider-pagination"></div>
+    </div>
+</div>
 
 <?php
 get_footer();
