7d692df00c290be55e76d73ac4d36ac126a52b4c
Павел Седой <pavliksedoy@gmail.com>
2019-05-07 12:02:43 +0300

    Logistic Page

--- wp-content/themes/twentynineteen/page-logistic.php
+++ wp-content/themes/twentynineteen/page-logistic.php
@@ -90,6 +90,41 @@ get_header();
 
     </div>
 
+
+    <!-- Footer Request -->
+    <div class="logistic__footer-request">
+        <div class="logistic__footer-request_bg"></div>
+
+        <div class="container">
+            <div class="current-avto__content-title page__title">
+                <h2>Остались вопросы?</h2>
+            </div>
+
+            <div class="logistic__footer-desc">Заполните форму и мы ответим на все Ваши вопросы!</div>
+
+            <form action="" class="logistic__request-form">
+                <div class="aside-order__input-wr">
+                    <input type="text" name="name" id="name" class="aside-order__input" placeholder="Имя">
+                    <img src="<?php bloginfo('template_url') ?>/images/car-search-icons/name-icon.svg" class="aside-order__input_img">
+                </div>
+
+                <div class="aside-order__input-wr">
+                    <input type="text" name="phone" id="phone" class="aside-order__input" placeholder="Телефон">
+                    <img src="<?php bloginfo('template_url') ?>/images/car-search-icons/phone-icon-icon.svg" class="aside-order__input_img">
+                </div>
+
+                <div class="aside-order__input-wr">
+                    <input type="email" name="email" id="email" class="aside-order__input" placeholder="E-mail">
+                    <img src="<?php bloginfo('template_url') ?>/images/3auto-page-icons/email-icon.svg" class="aside-order__input_img">
+                </div>
+
+                <button type="submit" class="aside-order__submit btn btn__color_blue btn__width_265 btn__height_50">Отправить</button>
+            </form>
+
+        </div>
+
+    </div>
+
 </div>
 
 <?php
